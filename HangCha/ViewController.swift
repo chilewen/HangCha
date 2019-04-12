20d19bed6b24db30b6ec2903ccc63d98044ff63f
chilewen <chilewen@126.com>
2019-04-12 09:37:24 +0800

    Initial Commit

---
+++ HangCha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HangCha
+//
+//  Created by chilezzz on 2019/4/12.
+//  Copyright © 2019 chilezzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
